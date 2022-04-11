89377eb1a477b26583e5267120bfdac68992b03f
Vishnu <77982092+vishnusai2183@users.noreply.github.com>
2022-04-11 12:06:34 +0530

    Create cscan.c

---
+++ OS/APRIL4/cscan.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int request[100], i, n, TotalHeadMoment = 0, head, max;
+
+    printf("Enter maximum head position\n");
+    scanf("%d", &max);
+
+    printf("Enter initial head position\n");
+    scanf("%d", &head);
+
+    printf("Enter the number of Requests\n");
+    scanf("%d", &n);
+    printf("Enter the Requests sequence\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &request[i]);
+    int min, x, y;
+    for (x = head; x <= max; x++)
+    {
+        for (y = 0; y < n; y++)
+        {
+            if (x == request[y])
+            {
+                printf("the head stopped at %d\n", x);
+            }
+        }
+    }
+
+    for (x = 0; x <= head; x++)
+    {
+        for (y = 0; y < n; y++)
+        {
+            if (x == request[y])
+            {
+                printf("the head stopped at %d\n", request[y]);
+                min = request[y];
+            }
+        }
+    }
+    int sum;
+    sum = (max - head) + (min - 0) + (max - 0);
+
+    printf("\nTotal seek time is %d", sum);
+
+    return 0;
+}
