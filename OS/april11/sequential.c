b24cd15449ad9c169e24a33df33f8e17296c29a5
Vishnu <77982092+vishnusai2183@users.noreply.github.com>
2022-04-11 15:50:21 +0530

    Create sequential.c

---
+++ OS/april11/sequential.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+void
+sequential (int files[])
+{
+  int flag = 0, temp = 0, start, len, x, y;
+  printf ("enter the starting position and length of file : ");
+  scanf ("%d%d", &start, &len);
+  for (x = start; x < (start + len); x++)
+    {
+      if (files[x] == 0)
+	{
+	  temp++;
+	}
+    }
+
+  if (temp == len)
+    {
+      printf ("the positions where file is stored is\n");
+      for (y = start; y < (start + len); y++)
+	{
+	  files[y] = 1;
+	  printf ("%d\n", y);
+	}
+      printf ("the file is allocated");
+    }
+  else
+    {
+      printf ("the file is not allocated");
+    }
+  int ch;
+  printf ("Do you want to enter more files?\n");
+  printf ("Press 1 for YES, 0 for NO: ");
+  scanf ("%d", &ch);
+  if (ch == 1)
+    sequential (files);
+  else
+    exit (0);
+
+
+
+}
+
+
+int
+main ()
+{
+  int files[50];
+  for (int i = 0; i < 50; i++)
+    {
+      files[i] = 0;
+    }
+
+  printf ("The allocated files are: ");
+  sequential (files);
+  return 0;
+}
