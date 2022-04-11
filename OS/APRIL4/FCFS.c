78f30c07b0d699a6c1ed7e3addac14ad6eae30c2
Vishnu <77982092+vishnusai2183@users.noreply.github.com>
2022-04-11 12:04:50 +0530

    Update FCFS.c

--- OS/APRIL4/FCFS.c
+++ OS/APRIL4/FCFS.c
@@ -1,1 +1,30 @@
-as
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int request[100],i,n,TotalHeadMoment=0,initial;
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the Requests sequence\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&request[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&initial);
+    
+    // logic for FCFS disk scheduling
+    printf("The sequence is : ");
+    for(i=0;i<n;i++)
+    {
+        TotalHeadMoment=TotalHeadMoment+abs(request[i]-initial);
+    
+      printf( "%d\t",request[i]);
+        initial=request[i];
+}
+    
+       
+
+printf("\nTotal head moment is %d",TotalHeadMoment);
+
+    return 0;
+    
+}
